02306ed6b4a3b638c4a4da6ac88a7f1af19f0c60
Mo <mouhamadou.diouf.1@gmail.com>
2025-05-16 13:00:29 +0000

    create get method

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 class HashMap {
-    constructor(){
-        this.loadFactor = 1;
-        this.capacity = 16;
-        this.buckets = [];
-    }
+  constructor() {
+    this.loadFactor = 0.75;
+    this.capacity = 16;
+    this.buckets = [];
+  }
 
   hash(key) {
-   let hashCode = 0;
-      
-   const primeNumber = 31;
-   for (let i = 0; i < key.length; i++) {
-     hashCode = primeNumber * hashCode + key.charCodeAt(i);
-   }
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % this.capacity;
+    }
+    return hashCode;
+  }
 
-   return hashCode;
- }
+  set(key, value) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.capacity) {
+      throw new Error("Trying to access index out of bounds");
+    }
+    this.buckets[index] = value;
+  }
 
-
- set(key, value){
-
- }
-
+  get(key){
+    const index = this.hash(key);
+    return this.buckets[index]
+  }
 }
+
+const myMap = new HashMap();
+myMap.set("name", "Mo");
+console.log(myMap);
